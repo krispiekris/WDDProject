20b4a5cfb5b9720f4c809f3ff22969ab8ef973e7
Athsof1 <athan.sofo@gmail.com>
2020-12-08 05:54:35 +0200

    Added StaySafe. -Alpha version

--- js/scripts.js
+++ js/scripts.js
@@ -68,3 +68,15 @@ for (i = 0; i < acc.length; i++) {
   });
 }
   
+
+
+
+
+//test2
+
+$(document).ready(function() {
+  $(".nav-link").click(function() {
+    $(".tab-content").find('.tab-pane').not(':eq('+$(this).index()+')').hide('slow');
+    $(".tab-content").find('.tab-pane').eq($(this).index()).toggle({ direction: "top" }, 2000);
+  });
+});
