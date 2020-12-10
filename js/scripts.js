7c66e25020244b68604ff27008b6187fe0bc6b4b
Athsof1 <athan.sofo@gmail.com>
2020-12-10 05:26:14 +0200

    Act carousel and modifications in Stay Safe

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-    document.getElementById("myDIV").onclick= function myFunction() 
+    document.getElementById("my2DIV").onclick= function my2Function() 
     {
       var smthng =document.getElementById("mainNavbar");
       smthng.classList.add("scrolled2");
@@ -84,4 +84,30 @@ for (i = 0; i < acc.length; i++) {
 
 
 //test2
+
+function openCategory(evt, Category1) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(Category1).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+
+// hiall
+
+
+
+
+
+
+
 
