43548be1244c4007a53bb46fb8f6f2a43e2c040e
christos <krito_pan@hotmail.com>
2020-12-11 18:38:22 +0200

    Act works

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,69 @@
 
-    document.getElementById("my2DIV").onclick= function my2Function() 
-    {
-      var smthng =document.getElementById("mainNavbar");
-      smthng.classList.add("scrolled2");
-    }
-    
-    
-    
-    $(function () {
-      $(document).scroll(function () {
-        var $nav = $("#mainNavbar");
-        
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height()); //when we reach a certain point
-        
-      })
-    })
-  
-    
+document.getElementById("my2DIV").onclick = function my2Function() {
+  var smthng = document.getElementById("mainNavbar");
+  smthng.classList.add("scrolled2");
+}
 
 
-    // cases
-    
-    let new_cases = document.getElementById("new_case");
-let new_death = document.getElementById("new_death");
-let total_death = document.getElementById("total_death");
-let total_recovered = document.getElementById("total_recovered");
-let total_cases = document.getElementById("total_cases");
-let table = document.getElementById('countries_stat')
-// Fetching the Data from the server
 
-//Fetching the World Data
-fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-        "x-rapidapi-key": "f93ff5e3afmsh1c47b09be99cc61p1ca087jsnbd673fdc9881"
-    }
-})
-.then(response => response.json().then( data => {
-    console.log(data);
-    total_cases.innerHTML = data.total_cases;
-    new_cases.innerHTML = data.new_cases;
-    new_death.innerHTML = data.new_deaths;
-    total_death.innerHTML = data.total_deaths;
-    total_recovered.innerHTML = data.total_recovered;
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("#mainNavbar");
 
-})).catch(err => {
-    console.log(err);
-});
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height()); //when we reach a certain point
 
-//Fetching The Case by Country Data
-fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-        "x-rapidapi-key": "f93ff5e3afmsh1c47b09be99cc61p1ca087jsnbd673fdc9881"
-    }
+  })
 })
 
-.catch(err => {
-    console.log(err);
-});
+
+
+
+// cases make a change
+
+
+// var new_cases = document.getElementById("new_case");
+// var new_death = document.getElementById("new_death");
+// var total_death = document.getElementById("total_death");
+// var total_recovered = document.getElementById("total_recovered");
+// var total_cases = document.getElementById("total_cases");
+// var table = document.getElementById('countries_stat')
+
+// fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
+//   "method": "GET",
+//   "headers": {
+//     "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+//     "x-rapidapi-key": "f93ff5e3afmsh1c47b09be99cc61p1ca087jsnbd673fdc9881"
+//   }
+// })
+//   .then(response => response.json().then(data => {
+//     console.log(data);
+//     total_cases.innerHTML = data.total_cases;
+//     new_cases.innerHTML = data.new_cases;
+//     new_death.innerHTML = data.new_deaths;
+//     total_death.innerHTML = data.total_deaths;
+//     total_recovered.innerHTML = data.total_recovered;
+
+//   })).catch(err => {
+//     console.log(err);
+//   });
+
+// //Fetching The Case by Country Data
+// fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
+//   "method": "GET",
+//   "headers": {
+//     "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+//     "x-rapidapi-key": "f93ff5e3afmsh1c47b09be99cc61p1ca087jsnbd673fdc9881"
+//   }
+// })
+
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+
+
+
+
 
 // test accordion
 
@@ -68,7 +71,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
@@ -78,7 +81,7 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-  
+
 
 
 
@@ -103,6 +106,7 @@ function openCategory(evt, Category1) {
 
 
 // hiall
+
 
 
 
