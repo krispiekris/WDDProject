41a2e8e78378c0183c72b92cad39f2f6587d6c9f
christos <krito_pan@hotmail.com>
2020-12-12 21:43:51 +0200

    almost 10 Saturday

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,6 @@ $(function () {
 
 
 
-// cases make a change
 
 
 
@@ -26,52 +25,9 @@ $(function () {
 
 
 
-// test accordion
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
 
 
 
-
-
-//test2
-
-function openCategory(evt, Category1) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(Category1).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-
-
-
-// destroyer of the universe
-$(".accordion2").on("click", ".accordion2-header", function() {
-  $(this).toggleClass("active").next().slideToggle();
-});
-
-document.getElementById("smthsmth").click();
 
 
 
